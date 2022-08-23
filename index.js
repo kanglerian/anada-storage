f618407dc5ae52965c193488dd2744d895177f32
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2022-08-23 13:07:45 +0700

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Anada Printing Storage');
+});
+
+app.listen(port, () => console.log(`Apps run on http://localhost:${port}`));
